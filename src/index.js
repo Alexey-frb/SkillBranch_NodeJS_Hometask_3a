676aaf9e5a83843a59bb706bcbc85958217f3138
Alexey Churkin <alexey.frb@gmail.com>
2016-11-12 22:37:34 +0300

    hometask 3a completed (variant 2)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import fetch from 'isomorphic-fetch';
 import cors from 'cors';
+import _ from 'lodash';
 
 const app = express();
 app.use(cors());
@@ -16,118 +17,6 @@ fetch(baseUrl)
   .catch((err) => {
     console.log('Чтото пошло не так:', err);
   });
-
-app.get('/task3a', (req, res) => {
-  res.json(pc);
-});
-
-app.get('/task3a/board', (req, res) => {
-  res.json(pc.board);
-});
-
-app.get('/task3a/board/vendor', (req, res) => {
-  res.json(pc.board.vendor);
-});
-
-app.get('/task3a/board/model', (req, res) => {
-  res.json(pc.board.model);
-});
-
-app.get('/task3a/board/cpu', (req, res) => {
-  res.json(pc.board.cpu);
-});
-
-app.get('/task3a/board/cpu/model', (req, res) => {
-  res.json(pc.board.cpu.model);
-});
-
-app.get('/task3a/board/cpu/hz', (req, res) => {
-  res.json(pc.board.cpu.hz);
-});
-
-app.get('/task3a/board/image', (req, res) => {
-  res.json(pc.board.image);
-});
-
-app.get('/task3a/board/video', (req, res) => {
-  res.json(pc.board.video);
-});
-
-app.get('/task3a/ram', (req, res) => {
-  res.json(pc.ram);
-});
-
-app.get('/task3a/ram/vendor', (req, res) => {
-  res.json(pc.ram.vendor);
-});
-
-app.get('/task3a/ram/volume', (req, res) => {
-  res.json(pc.ram.volume);
-});
-
-app.get('/task3a/ram/pins', (req, res) => {
-  res.json(pc.ram.pins);
-});
-
-app.get('/task3a/os', (req, res) => {
-  res.json(pc.os);
-});
-
-app.get('/task3a/floppy', (req, res) => {
-  res.json(pc.floppy);
-});
-
-app.get('/task3a/hdd', (req, res) => {
-  res.json(pc.hdd);
-});
-
-app.get('/task3a/hdd/:id', (req, res) => {
-  const id = req.params.id;
-  const hdds = pc.hdd;
-
-  if (id >= 0 && id < hdds.length) {
-    res.json(hdds[id]);
-  } else {
-    res.status(404).send('Not found');
-  }
-});
-
-app.get('/task3a/hdd/:id/vendor', (req, res) => {
-  const id = req.params.id;
-  const hdds = pc.hdd;
-
-  if (id >= 0 && id < hdds.length) {
-    res.json(hdds[id].vendor);
-  } else {
-    res.status(404).send('Not found');
-  }
-});
-
-app.get('/task3a/hdd/:id/size', (req, res) => {
-  const id = req.params.id;
-  const hdds = pc.hdd;
-
-  if (id >= 0 && id < hdds.length) {
-    res.json(hdds[id].size);
-  } else {
-    res.status(404).send('Not found');
-  }
-});
-
-app.get('/task3a/hdd/:id/volume', (req, res) => {
-  const id = req.params.id;
-  const hdds = pc.hdd;
-
-  if (id >= 0 && id < hdds.length) {
-    res.json(hdds[id].volume);
-  } else {
-    res.status(404).send('Not found');
-  }
-});
-
-app.get('/task3a/monitor', (req, res) => {
-  res.json(pc.monitor);
-});
 
 app.get('/task3a/volumes', (req, res) => {
   const hdds = pc.hdd;
@@ -154,9 +43,31 @@ app.get('/task3a/volumes', (req, res) => {
   });
 });
 
-app.use((req, res) => {
-  res.status(404).send('Not found');
-  return;
+app.get('/task3a/:id1?/:id2?/:id3?', (req, res) => {
+  const id1 = req.params.id1;
+  const id2 = req.params.id2;
+  const id3 = req.params.id3;
+  let result;
+
+  try {
+    if (id3 != null) {
+      result = _.result(_.result(_.result(pc, [id1]), [id2]), [id3], 'Not found');
+    } else if (id2 != null) {
+      result = _.result(_.result(pc, [id1]), [id2], 'Not found');
+    } else if (id1 != null) {
+      result = _.result(pc, [id1], 'Not found');
+    } else {
+      result = pc;
+    }
+
+    if (result !== 'Not found') {
+      res.json(result);
+    } else {
+      res.status(404).send('Not found');
+    }
+  } catch (e) {
+    res.status(404).send('Not found');
+  }
 });
 
 app.listen(3000, () => {
